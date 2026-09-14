4c7e4f786411a588b0d62039ba3af46645cc7d0a
Florence Fok <30338770+ffok123@users.noreply.github.com>
2026-09-14 09:36:37 +0800

    hihi

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ask for Name</title>
+</head>
+<body>
 
-echo "Hello Azure!";
+    <form method="POST" action="">
+        <label>Please enter your name:</label>
+        <input type="text" name="username">
+        <button type="submit">Submit</button>
+    </form>
 
-?>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = htmlspecialchars($_POST['username']);
+        if (!empty($name)) {
+            echo "<h3>Hello, " . $name . "!</h3>";
+        } else {
+            echo "<p>Please type a valid name.</p>";
+        }
+    }
+    ?>
+
+</body>
+</html>
